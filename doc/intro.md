e11c7b7e81dad19b45e2c88ed58cf6d658045494
shark8me <kiran.karkera@gmail.com>
2024-02-09 19:04:51 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vani-jairam-qa
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/vani_jairam_qa/core.clj
@@ -0,0 +1,7 @@
+(ns vani-jairam-qa.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
